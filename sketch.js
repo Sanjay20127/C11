6cc9cc9d4c53f18e54b4714e8176cd74ce6ab5ca
Sanjay20127 <86659073+Sanjay20127@users.noreply.github.com>
2021-07-26 12:38:33 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+var pathImg,runAnime,path,player,rightBound,leftBound;
+function preload()
+{
+  //pre-load images
+  pathImg= loadImage("path.png");
+  runAnime= loadAnimation("Runner-1.png","Runner-2.png");
+}
+function setup()
+{
+  createCanvas(400,400);
+  //create sprites here
+  path=createSprite(200,200);
+  path.addImage(pathImg);
+  path.velocityY=4;
+  path.scale=1.2;
+  player=createSprite(200,350,50,50);
+  //player.loadAnimation(runAnime);
+  leftBound=createSprite(0,200,80,400);
+  rightBound=createSprite(400,200,75,400);
+  leftBound.visible=false;
+  rightBound.visible=false;
+}
+
+function draw()
+{
+  background(0);
+  if(path.y > 400)
+  {
+    path.y= height/2;
+  }
+  player.x=World.mouseX;
+  player.collide(leftBound);
+  player.collide(rightBound);
+  drawSprites();
+}
