33d1b50f3eac907ac5d443fc839690456353cf3e
Sanjay20127 <86659073+Sanjay20127@users.noreply.github.com>
2021-11-03 19:38:53 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -14,7 +14,8 @@ function setup()
   path.velocityY=4;
   path.scale=1.2;
   player=createSprite(200,350,50,50);
-  //player.loadAnimation(runAnime);
+  player.addAnimation("BoyRunning",runAnime);
+  player.scale=0.08;
   leftBound=createSprite(0,200,80,400);
   rightBound=createSprite(400,200,75,400);
   leftBound.visible=false;
